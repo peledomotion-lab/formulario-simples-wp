410399cfa806c86404bc1e82492fa71ecd542601
peledomotion-lab <peledomotion@gmail.com>
2026-04-29 20:46:09 -0300

    Create formulario-simples.php

---
+++ formulario-simples.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Plugin Name: Formulário Simples GPT
+ * Description: Um formulário básico criado com ajuda do GPT
+ */
+
+if (!defined('ABSPATH')) exit;
+
+function fs_mostrar_formulario() {
+    ob_start();
+    ?>
+
+    <form method="post">
+        <p>
+            <label>Nome:</label><br>
+            <input type="text" name="fs_nome" required>
+        </p>
+
+        <p>
+            <label>Email:</label><br>
+            <input type="email" name="fs_email" required>
+        </p>
+
+        <p>
+            <button type="submit" name="fs_enviar">Enviar</button>
+        </p>
+    </form>
+
+    <?php
+
+    if (isset($_POST['fs_enviar'])) {
+        $nome = sanitize_text_field($_POST['fs_nome']);
+        $email = sanitize_email($_POST['fs_email']);
+
+        echo "<p><strong>Recebido:</strong><br>Nome: $nome <br>Email: $email</p>";
+    }
+
+    return ob_get_clean();
+}
+
+add_shortcode('formulario_simples', 'fs_mostrar_formulario');
